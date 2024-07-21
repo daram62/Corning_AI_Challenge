0ed76e2b6caddf73490855e8417d1d2687cd9eb2
daram62 <97465078+daram62@users.noreply.github.com>
2024-07-22 01:34:58 +0900

    Update assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = "asst_lODAYULRvKKagqKNplujGEhD"; // set your assistant ID here
+export let assistantId = "asst_DqhSLGMU79k23sZjTFk8uvcC"; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
